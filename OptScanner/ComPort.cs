c31878616a638678785dfbf09ff6ea77d4eef97a
Kalyan00 <kalyan00@gmail.com>
2009-10-11 17:01:07 +0000

    OptScanner

---
+++ OptScanner/ComPort.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO.Ports;
+using System.Timers;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace OptScanner
+{
+	public class ComPort
+	{
+		private byte[] Enter = new byte[2] { 10, 13 };
+		private bool sleep;
+		private System.Windows.Forms.Control _Control;
+		private TimerHandler th = new TimerHandler();
+
+		public event EventHandler Recive;
+
+		public SerialPort ComPortInternal { get; set; }
+		public int WaitInterval { get; set; } 
+
+		public ComPort()
+		{
+			ComPortInternal = new SerialPort("COM1");
+		}
+
+		public void WaitResponse()
+		{
+			lock (Enter)
+			{
+				sleep = true;
+				while (sleep)
+				{
+					if (!_Control.Visible)
+						throw new Exception();
+					Thread.Sleep(100);
+					Application.DoEvents();
+				}
+			}
+		}
+		public string Ask(string Text)
+		{
+			Say(Text);
+			WaitResponse();
+			return ComPortInternal.ReadExisting();
+		}
+		public void Say(string Text)
+		{
+			ComPortInternal.Write(Text);
+			ComPortInternal.Write(Enter,0,2);
+		}
+
+
+		public void Init(System.Windows.Forms.Control control)
+		{
+			ComPortInternal.Open();
+			
+			ComPortInternal.DataReceived += comReceived;
+			_Control = control;
+		}
+		private void comReceived(object sender, SerialDataReceivedEventArgs e)
+		{
+			th.DoAfter(_Control, WaitInterval, comReceivedInvoker);
+		}
+
+		private void comReceivedInvoker()
+		{
+			if (sleep)
+			{
+				sleep = false;
+				return;
+			}
+			if (Recive != null)
+				Recive(this, new EventArgs());
+		}
+
+
+		public string ReadExisting()
+		{
+			return ComPortInternal.ReadExisting();
+		}
+	}
+}
---
+++ OptScanner/frmSelectUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OptScanner
+{
+	public partial class frmSelectUser : Form
+	{
+		public frmSelectUser()
+		{
+			InitializeComponent();
+		}
+
+		internal void Init(List<string> list)
+		{
+			comboBox1.Items.AddRange(list.ToArray());
+		}
+
+		internal string SelectedName()
+		{
+			return comboBox1.SelectedItem.ToString();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.OK;
+			Close();
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			button1.Enabled = true;
+		}
+	}
+}
