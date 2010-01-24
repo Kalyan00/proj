72183c563f9b35a99aced49856da168f2867ec4c
Kalyan00 <kalyan00@gmail.com>
2010-01-24 21:07:36 +0000

    полностью переписал меню.  + обошел кеширование css + заглушка стиля меню для IE.

--- www/js_cms/main.js
+++ www/js_cms/main.js
@@ -30,167 +30,57 @@ var menuGlobal = new Array(new Array("Меню","", new Array(
 	))
 )));
 
-
-
-var timer;
-var menu;
-function scroll()
-{
-	//alert('scroll');
-}
-
 function createCss()
 {
 	var lnk=document.createElement('link');
 	lnk.rel="stylesheet"; 
 	lnk.type="text/css";
-    lnk.href="css.css";
+    lnk.href="css.css?"+Math.random();
 	var head=document.getElementsByTagName("head")[0];
 	head.appendChild(lnk);
 }
-function createMenu()
-{
-	menu=document.createElement('div');
-	menu.id="menuF";
-	menu.className="menuPassive";
-	
-	var menug = createMenu2(menuGlobal)
-	menug.style.display="block";
-	menu.appendChild(menug);
-	
-	
-	menu.onmouseover=function ()
-	{
-		//this.style.backgroundColor = "#CCCAAA";
-		this.className ="menuActive";
-		clearTimeout(timer);
-	};
-	menu.onmouseout=function ()
-	{
-		this.className ="menuPassive";
-		//this.style.backgroundColor ="transparent"; 
-		timer = setTimeout(function()
-		{
-			if(menug.lastChild.lastChild.lastChild.lastChild.style.display!="none")
-				menug.firstChild.firstChild.firstChild.firstChild.onclick();
-		}, 300);
-	};
-	document.body.appendChild(menu);
-}
-function MenuItemClick()
-{
-	if(this.nextSibling.style.display =="none")
-	{
-		this.nextSibling.style.display = "block";
-		this.firstChild.firstChild.firstChild.firstChild.firstChild.src="minus.gif";
-	}
-	else
-	{
-		
-		this.firstChild.firstChild.firstChild.firstChild.firstChild.src="plus.gif";
-		this.nextSibling.style.display = "none";
-		if(navigator.appName == "Opera") // в старой версии оперы глюка с автошириной. но эта поправка портит ширину в IE
-		{
-			menu.style.width=10;
-			menu.style.width="inherit";
-		}
-		
-	}
-	
-}
 
-function AddDots(menu)
+function createDocument()
 {
-	var tr = document.createElement('tr');
-	var td = document.createElement('td');
-	
-	td.style.width=10;
-	tr.appendChild(td);
-	td = document.createElement('td');
-	td.appendChild(menu);
-	tr.appendChild(td);
-	var tbody = document.createElement('tbody');
-	tbody.appendChild(tr);
-	var table = document.createElement('table');
-	table.appendChild(tbody);
-	table.style.display ="none";
-	return table;	
-	
-}
-function createMenu2(arr)
+	body = document.getElementsByTagName('body')[0];
+	body.innerHTML=''
+		+'<div id="mainDiv">' 
+		+body.innerHTML
+		+'<br style="clear:both">'
+		+'</div>'
+		+getMenuText()
+		;
+	maindiv=document.getElementById('mainDiv');
+
+};
+
+function getMenuText(arr, clas)
 {
-	if(!arr||!arr.length)
-		return document.createElement('table');
-	var menu=document.createElement('table');
-	
-	menu.className = "menuTable";
-	
-	var tbody = document.createElement('tbody');
+	var mainMenuClass = 'mainMenu';
+	if(navigator.appName == "Microsoft Internet Explorer")
+		mainMenuClass = 'mainMenuIE';
+	if(!arr)
+		return '<div id="'+mainMenuClass+'"><ul>'+getMenuText(menuGlobal[0][2],'top')+'</ul></div>';
+	var result='';
 	for(var i=0;i<arr.length;i++)
 	{
-		var tr=document.createElement('tr');
-		var td=document.createElement('td');
-		var item = document.createElement('font');
-		var innerMenu = null;
-		if (arr[i].length)
-		{
-			var text = "<table><tr><td width=10px>";
-			var innertext= arr[i][0];
-			if(arr[i].length>2)
-			{
-				text += "<img src='plus.gif'>";
-				innerMenu = createMenu2(arr[i][2]);
-				innerMenu = AddDots(innerMenu);
-			}
-			else 
-				innertext="<a href='"+arr[i][1]+"'>"+arr[i][0]+"</a>";
-			if(arr[i].length==1)
-				innertext = arr[i][0];
-			text+="</td><td>"+innertext+"</td></tr><table>";
-			item.innerHTML=text;
-		}
-		else
-			item.innerHTML = 'item'+i;
-	
-		td.appendChild(item);
-		if(innerMenu)
-		{
-			item.onclick=MenuItemClick;
-			td.appendChild(innerMenu);
-		}
-		tr.appendChild(td);
-		tbody.appendChild(tr);
-	}
-	menu.appendChild(tbody);
-	return menu;
-}
+		result+='<li class="menuElem '+clas+(arr[i][2]?' fly':'')+'"><a';
+		if(arr[i][1])
+			result+=' href="'+arr[i][1]+'"';
+		result+='><span>'+arr[i][0]+'</span></a>';
+		if(arr[i][2])
+			result+='<ul class="menuGroup">'+getMenuText(arr[i][2],'sub')+'</ul>';
+		result+='</li>'
+	}	
+	return result;
+};
 
-function createBody()
-{
-	var mainDiv=document.createElement('div');
-	mainDiv.id='mainDiv';
-	var body = document.getElementsByTagName("body")[0];
-	for(;body.firstChild; )
-	{
-		var el = body.firstChild;
-		body.removeChild(el);
-		mainDiv.appendChild(el);
-	}
-	var lastBr = document.createElement('br');
-	lastBr.style.clear = 'both';
-	mainDiv.appendChild(lastBr);
-	body.appendChild(mainDiv);
-	
-}
-
-function load()
+function start()
 {
 	createCss();
-	createBody();
-	createMenu();
-}
+	createDocument();	
+};
+document.write('<body onload="start();">');
 if(document.getElementsByTagName("head").length == 0) document.write("<head/>");
-window.onscroll =scroll;
-window.onload =load;
 
-// </script>
+
