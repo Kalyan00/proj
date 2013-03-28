4bc2ba4625e9a8dbc2be96693d3451a478b1bcd6
kalyan00@gmail.com <kalyan00@gmail.com@40912b6d-d52e-0410-9514-1fe0b686eeb0>
2013-03-28 20:20:50 +0000

    puppet service

--- trunk/puppets/pi/puppetService.pp
+++ trunk/puppets/pi/puppetService.pp
@@ -1,7 +1,5 @@
-file { '/tmp/helloworld':
-    ensure  => present,          # файл должен существовать
-    content => 'Hello, world!',  # содержимым файла должна являться строка "Hello, world!"
-    mode    => 0644,             # права на файл - 0644
-    owner   => 'root',           # владелец файла - root
-    group   => 'root'            # группа файла - root
+cron { puppetService:
+  command => "/root/puppets/pup-check -update > /root/pup.log",
+  user    => root,
+  minute  => '*/1'
 }
