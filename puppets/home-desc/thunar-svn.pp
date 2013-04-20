d44c3948318f63e746c000dcf5bbc2bdc99d1268
kalyan00 <kalyan00@gmail.com>
2013-04-20 16:38:05 +0000

    remove double

--- puppets/home-desc/thunar-svn.pp
+++ puppets/home-desc/thunar-svn.pp
@@ -4,15 +4,3 @@ package { 'rabbitvcs-cli':}
 package { 'thunar-vcs-plugin':}
 
 
-
-apt::ppa { "ppa:webupd8team/sublime-text-2": }
-package { 'sublime-text':
-    ensure => latest
-}
-
-
-package { 'mc':
-    ensure => latest
-}
-
-
