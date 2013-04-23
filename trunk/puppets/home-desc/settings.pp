4df10463e27a4aa869c6cac4c761e016037bc4dc
kalyan00 <kalyan00@40912b6d-d52e-0410-9514-1fe0b686eeb0>
2013-04-23 21:22:11 +0000

    setup.pi

--- trunk/puppets/home-desc/settings.pp
+++ trunk/puppets/home-desc/settings.pp
@@ -4,8 +4,8 @@
   package { 'numlockx':}
 
   exec { "set numlockx on":
-         command => "sed -i 's|^exit 0.*$|# Numlock enable\n[ -x /usr/bin/numlockx ] \&\& numlockx on\n\nexit 0|' /etc/rc.local",
-        unless => "grep -qe '^\[ -x /usr/bin/numlockx \] \&\& numlockx on' -- /etc/rc.local",
+         command => "sed -i 's|^exit 0.*$|# Numlock enable\\n[ -x /usr/bin/numlockx ] \\&\\& numlockx on\\n\\nexit 0|' /etc/rc.local",
+        unless => "grep -qe '^\\[ -x /usr/bin/numlockx \\] \\&\\& numlockx on' -- /etc/rc.local",
         path => "/bin:/usr/bin:/usr/local/bin",
   }
 
