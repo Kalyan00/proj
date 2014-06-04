1836296204e1c2aad0bde80db42b952b5816f98c
kalyan00@gmail.com <kalyan00@gmail.com@40912b6d-d52e-0410-9514-1fe0b686eeb0>
2014-06-04 19:41:46 +0000

    -

--- trunk/puppets/pi/btsync.pp
+++ trunk/puppets/pi/btsync.pp
@@ -1,4 +1,4 @@
 exec { "install btsync":
-    command => "[ -e ~/.btsync ] || mkdir ~/.btsync && cd ~/.btsync && wget http://btsync.s3-website-us-east-1.amazonaws.com/btsync_arm.tar.gz && tar -xvf btsync_arm.tar.gz || cd ~ && rm -rf ~/.btsync && [ -e ~/.btsync ]",
+    command => "[ -e ~/.btsync ] || mkdir ~/.btsync && cd ~/.btsync && wget http://btsync.s3-website-us-east-1.amazonaws.com/btsync_arm.tar.gz && tar -xvf btsync_arm.tar.gz || cd ~ && rm -rf ~/.btsync",
     path => "/bin:/usr/bin:/usr/local/bin"
 }
