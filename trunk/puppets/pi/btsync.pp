e3e95ecb19990b252341ddef10e7c69289e491d3
kalyan00@gmail.com <kalyan00@gmail.com@40912b6d-d52e-0410-9514-1fe0b686eeb0>
2014-06-10 22:08:54 +0000

    create_daemon {'btsync':

--- trunk/puppets/pi/btsync.pp
+++ trunk/puppets/pi/btsync.pp
@@ -1,4 +1,24 @@
-exec { "install btsync":
-    command => "bash -c '[ -e ~/.btsync ] || mkdir ~/.btsync && cd ~/.btsync && wget http://btsync.s3-website-us-east-1.amazonaws.com/btsync_arm.tar.gz && tar -xvf btsync_arm.tar.gz || cd ~ && rm -rf ~/.btsync '",
-    path => "/bin:/usr/bin:/usr/local/bin"
+
+file { '~/.btsync/install':
+    ensure  => present,          # файл должен существовать
+    content => regsubst('#!/bin/bash
+
+        cd ~/.btsync && wget http://btsync.s3-website-us-east-1.amazonaws.com/btsync_arm.tar.gz && tar -xvf btsync_arm.tar.gz || cd ~ && rm -rf ~/.btsync 
+        
+        ', '\x0d', '', 'G'),
+    mode    => 0700, 
+    owner   => 'root',
+    group   => 'root'  
 }
+
+exec { "install btsync":
+    command => "~/.btsync/install",
+    path => "/bin:/usr/bin:/usr/local/bin",
+    creates => "~/.btsync/btsync"
+}
+
+create_daemon {'btsync':
+    $name='btsync',
+    $path = "/root/.btsync/"
+}
+
