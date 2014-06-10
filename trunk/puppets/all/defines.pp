dac08fa0d40fd1e1d44b1b6e48146d65c36cf58c
kalyan00@gmail.com <kalyan00@gmail.com@40912b6d-d52e-0410-9514-1fe0b686eeb0>
2014-06-10 22:00:32 +0000

    define create_daemon

--- trunk/puppets/all/defines.pp
+++ trunk/puppets/all/defines.pp
@@ -1,5 +1,4 @@
 
-
 define ensure_key_value($file, $key, $value, $delimiter = " ") {
     # append line if "$key" not in "$file"
     exec { "append $key$delimiter$value $file":
@@ -15,4 +14,122 @@ define ensure_key_value($file, $key, $value, $delimiter = " ") {
         unless => "grep -xqe '$key$delimiter$value' -- $file",
         path => "/bin:/usr/bin:/usr/local/bin"
     }
+}
+
+
+
+
+define create_daemon($name, $args = "", $path = "/usr/sbin/"){
+    file { "/etc/init.d/$name":
+        ensure  => present,  
+        content => regsubst('#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          skeleton
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: $name daemon
+# Description:       $path$name $args daemon, created by create_daemon.pp
+#
+### END INIT INFO
+
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+DESC="$path$name $args daemon"
+NAME=$name
+DAEMON=$path$NAME
+DAEMON_ARGS="$args"
+PIDFILE=/var/run/$NAME.pid
+SCRIPTNAME=/etc/init.d/$NAME
+
+[ -x "$DAEMON" ] || exit 0
+[ -r /etc/default/$NAME ] && . /etc/default/$NAME
+. /lib/init/vars.sh
+. /lib/lsb/init-functions
+
+do_start()
+{
+
+	start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON --test > /dev/null \
+		|| return 1
+	start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON -- \
+		$DAEMON_ARGS \
+		|| return 2
+
+}
+
+
+do_stop()
+{
+
+	start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --pidfile $PIDFILE --name $NAME
+	RETVAL="$?"
+	[ "$RETVAL" = 2 ] && return 2
+
+	start-stop-daemon --stop --quiet --oknodo --retry=0/30/KILL/5 --exec $DAEMON
+	[ "$?" = 2 ] && return 2
+
+	rm -f $PIDFILE
+	return "$RETVAL"
+}
+
+
+do_reload() {
+
+	start-stop-daemon --stop --signal 1 --quiet --pidfile $PIDFILE --name $NAME
+	return 0
+}
+
+case "$1" in
+  start)
+	[ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
+	do_start
+	case "$?" in
+		0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
+		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+	esac
+	;;
+  stop)
+	[ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
+	do_stop
+	case "$?" in
+		0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
+		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+	esac
+	;;
+  status)
+       status_of_proc "$DAEMON" "$NAME" && exit 0 || exit $?
+       ;;
+
+  restart|force-reload)
+
+	log_daemon_msg "Restarting $DESC" "$NAME"
+	do_stop
+	case "$?" in
+	  0|1)
+		do_start
+		case "$?" in
+			0) log_end_msg 0 ;;
+			1) log_end_msg 1 ;; # Old process is still running
+			*) log_end_msg 1 ;; # Failed to start
+		esac
+		;;
+	  *)
+	  	# Failed to stop
+		log_end_msg 1
+		;;
+	esac
+	;;
+  *)
+	echo "Usage: $SCRIPTNAME {start|stop|status|restart|force-reload}" >&2
+	exit 3
+	;;
+esac
+
+:
+             ', '\x0d', '', 'G'),
+        mode    => 0700, 
+        owner   => 'root',
+        group   => 'root'  
+    }    
 }
