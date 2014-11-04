b151f46e842b19e48da17f86775d0b7d5c3a3e1b
kalyan00 <kalyan00@gmail.com>
2014-11-04 17:03:24 +0000

    openvpnd

--- puppets/pi/openvpn.pp
+++ puppets/pi/openvpn.pp
@@ -2,9 +2,21 @@ package { 'openvpn':
     ensure => latest
 }
 
+file { '/root/.openvpn/':
+    ensure  => present,          # файл должен существовать
+    content => regsubst('#!/bin/bash
+
+        openvpn --config "/root/.openvpn/n.kolomeitsev.ovpn" --askpass "/root/.openvpn/.p"
+        
+        ', '\x0d', '', 'G'),
+    mode    => 0700, 
+    owner   => 'root',
+    group   => 'root',
+}
+
 create_daemon {'openvpnd':
-    name => "openvpn",
-    args => "--config '/root/.openvpn/n.kolomeitsev.ovpn' --askpass '/root/.openvpn/.p'",
+    name => "openvpnd",
+    path => "/root/.openvpn/",
     require => package["openvpn"]
 }
 
